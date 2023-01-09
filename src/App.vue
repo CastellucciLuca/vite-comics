41669990d3b5fb4ea8215e3a544bd0447e86aaa4
CastellucciLuca <castellucciluca98@gmail.com>
2023-01-09 15:05:50 +0100

    add-header structure

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,28 @@
-<script setup>
-
+<script>
+import AppHeader from './components/AppHeader.vue';
+export default {
+  components: {
+    AppHeader,
+  }
+}
 </script>
 
+
+
 <template>
+  <!-- Import Header -->
+  <AppHeader />
+
+  <!-- Import Main -->
+  <AppMain />
 
 </template>
 
-<style scoped>
-
+<style lang="scss">
+@use './styles/general.scss' as *;
+div.container-wrapped {
+  width: 80%;
+  border: 1px solid green;
+  margin: 0 auto;
+}
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+}
+</script>
+<template>
+    <header>
+        <div class="container-wrapped">
+            <img src="../assets/dc-logo.png" alt="DC Main Logo">
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">Item</a>
+                    </li>
+                    <li>
+                        <a href="#">Item</a>
+                    </li>
+                    <li>
+                        <a href="#">Item</a>
+                    </li>
+                    <li>
+                        <a href="#">Item</a>
+                    </li>
+                    <li>
+                        <a href="#">Item</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+
+    </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: .5rem;
+}
+img {
+    height: 80%;
+}
+nav ul li {
+    display: inline;
+    padding: 0 1rem;
+}
+</style>
