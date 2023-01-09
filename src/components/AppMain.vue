c9978a13fe645d6998e3fa93b9b56947ab684625
CastellucciLuca <castellucciluca98@gmail.com>
2023-01-09 15:10:43 +0100

    add-main-structure

---
+++ src/components/AppMain.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+}
+</script>
+
+<template>
+    <main>
+        <div class="container-wrapped">
+
+            <section id="jumbotron">
+                <h2> Content Goes Here </h2>
+            </section>
+        </div>
+    </main>
+</template>
+
+
+<style lang="scss" scoped>
+main {
+    background-color: black;
+    color: white;
+    padding: 2rem;
+}
+</style>
